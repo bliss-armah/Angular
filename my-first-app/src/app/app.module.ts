eedd132fb9ffe742e4c636862256c421eabbe0c8
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-08-02 15:09:48 +0000

    components created

---
+++ my-first-app/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { SeversComponent } from './severs/severs.component';
+
+@NgModule({
+  declarations: [AppComponent, ServerComponent, SeversComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ my-first-app/src/app/severs/severs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-severs',
+  templateUrl: './severs.component.html',
+  styleUrls: ['./severs.component.css']
+})
+export class SeversComponent {
+
+}
