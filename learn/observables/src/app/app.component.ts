e763cad6726b1f40a285c587d0cf2dbb181b535f
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2024-02-09 09:34:42 +0000

    observables added

---
+++ learn/observables/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false;
+  private activatedSubscription: Subscription;
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.activatedSubscription = this.userService.activatedEmitter.subscribe(
+      (didActivate) => {
+        this.userActivated = didActivate;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.activatedSubscription.unsubscribe();
+  }
+}
